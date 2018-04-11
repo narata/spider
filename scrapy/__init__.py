c190b021e8fa6df8dbb1097a8dd61a87b922052e
mac <mac@narata.com>
2018-04-12 00:14:48 +0800

    首次学习scrapy

---
+++ scrapy/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/4/11 下午10:48
+# @Author  : Narata
+# @File    : __init__.py.py
+# @Software: PyCharm
+
---
+++ scrapy/tutorial/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/4/11 下午11:19
+# @Author  : Narata
+# @File    : __init__.py.py
+# @Software: PyCharm
+

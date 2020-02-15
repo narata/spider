5b059f5a5587359ede5748622e0541b0fe2dd290
narata <jtnarata@163.com>
2020-02-15 20:48:56 +0800

    add novel xbookcn

--- novel/xbookcn/demo.py
+++ novel/xbookcn/demo.py
@@ -1,6 +1,7 @@
 from urllib import request
 from lxml import etree
 import re
+import os
 
 headers = {
     'User_Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0',
@@ -9,20 +10,34 @@ headers = {
     'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
 }
 
-for i in range(62, 999):
-    try:
-        url = 'https://www.xbookcn.net/read/story'+str(i).zfill(3)+'.htm'
-        print(url)
-        req = request.Request(url, headers=headers)
-        resp = request.urlopen(req)
-        html = resp.read().decode('gbk')
-        # print(html)
-        test = etree.HTML(html)
-        header = test.xpath("//h3/text()")[0]
-        text = test.xpath("//p/text()")
-        with open("data/" + header + ".txt", "w") as fp:
-            fp.write("".join(text))
-        print(header)
-    except Exception as e:
-        print(e.message)
-
+url = "https://www.xbookcn.net/read/"
+req = request.Request(url, headers=headers)
+resp = request.urlopen(req)
+html = resp.read().decode('gbk')
+test = etree.HTML(html)
+class_names = test.xpath("//a/text()")
+class_urls = test.xpath("//a/@href")
+for i in range(0, 17):
+    if not os.path.exists("data/{}".format(class_names[i])):
+        os.makedirs("data/{}".format(class_names[i]))
+    class_url = "https://www.xbookcn.net/read/" + class_urls[i]
+    req_class = request.Request(class_url, headers=headers)
+    resp_class = request.urlopen(req)
+    html_class = resp_class.read().decode('gbk')
+    test_class = etree.HTML(html_class)
+    novel_urls = test_class.xpath("//a/@href")
+    novel_names = test_class.xpath("//a/text()")
+    for j in range(0, len(novel_urls)):
+        if "story" in novel_urls[j]:
+            try:
+                novel_url = "https://www.xbookcn.net/read/" + novel_urls[i]
+                req_novel = request.Request(class_url, headers=headers)
+                resp_novel = request.urlopen(req)
+                html_novel = resp_novel.read().decode('gbk')
+                test_novel = etree.HTML(html_novel)
+                text = test.xpath("//p/text()")
+                with open("data/{}/{}.txt".format(class_names[i], novel_names[j]), "w") as fp:
+                    fp.write("".join(text))
+                print(novel_names[j])
+            except Exception as e:
+                print(e.message)

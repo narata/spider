0c5d7a850bc9590ee395c4a1d55e661871528fa9
narata <jtnarata@163.com>
2020-02-18 14:29:57 +0800

    h528

--- novel/h528/demo.py
+++ novel/h528/demo.py
@@ -9,11 +9,25 @@ headers = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
     'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
 }
+path = "data/人妻熟女"
 
-url = "http://w2.h528.com/post/category/%e4%ba%ba%e5%a6%bb%e7%86%9f%e5%a5%b3"
-req = request.Request(url, headers=headers)
-resp = request.urlopen(req)
-html = resp.read().decode('utf-8')
-test = etree.HTML(html)
-urls = test.xpath("//div[@class='post']/table/tbody/td/h3/a/@href")
-print(urls)
+if not os.path.exists(path):
+    os.makedirs(path)
+
+for i in range(1, 177):
+    url = "http://w2.h528.com/post/category/%e4%ba%ba%e5%a6%bb%e7%86%9f%e5%a5%b3/page/" + str(i)
+    req = request.Request(url, headers=headers)
+    resp = request.urlopen(req)
+    html = resp.read().decode('utf-8')
+    test = etree.HTML(html)
+    urls = test.xpath("//a[@rel='bookmark']/@href")
+    names = test.xpath("//a[@rel='bookmark']/text()")
+    for j in range(0, len(urls)):
+        req_novel = request.Request(urls[j], headers=headers)
+        resp_novel = request.urlopen(req_novel)
+        html_novel = resp_novel.read().decode('utf-8')
+        test_novel = etree.HTML(html_novel)
+        text = test_novel.xpath("//p/text()")
+        with open(path + "/{}.txt".format(names[j]), "w") as fp:
+            fp.write("".join(text))
+        print(names[j])
